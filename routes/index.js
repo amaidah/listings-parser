17863e7af03ada1edcf632f5286692331d82c18c
Andrew Maidah <a.maidah@gmail.com>
2017-10-02 17:27:52 -0700

    starts parsing work
    Signed-off-by: Andrew Maidah <a.maidah@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 import express from 'express';
+import request from 'request';
+import fs from 'fs';
+import cheerio from 'cheerio';
 
 const router = express.Router();
+
+const readTestFile = (file, cb) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    cb(data);
+  })
+}
 
 router.get('/', (req, res, next) => {
   res.render('index');
 })
+
+router.get('/request', (req, res, next) => {
+  const { val } = req.query;
+  const resObj = {};
+  resObj.url = `${val}`;
+
+  // request(val, (error, response, body) => {
+  //   resObj.body = body;
+
+  //   fs.writeFile('listing.html', body, (err) => {
+  //     if (err) throw err;
+  //     console.log('The file has been saved!');
+  //   });
+
+  readTestFile('listing.html', file => {
+    resObj.parse = {};
+
+    // load html
+    let ch = cheerio.load(file, { ignoreWhiteSpace: true });
+
+    //start parse
+    let full_name = ch('.biz-page-title').html();
+    resObj.parse.full_name = full_name.trim();
+
+    let budget = ch('.business-attribute, .price-range').html();
+    resObj.parse.budget = budget.length;
+
+    let phone_number = ch('.biz-phone').html();
+    resObj.parse.phone_number = phone_number.replace(/\D+/g, "");
+
+    res.send(resObj);
+  })
+
+  // })
+
+})
+
 
 export default router;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
-const text = 'MEHN boiler!';
+const $indexForm = document.querySelector('.index-form'),
+      $indexInput = document.querySelector('#index-input');
 
-const welcome = () => {
-  console.log(`Welcome to ${text}`);
+const handleFormSubmit = evt => {
+  evt.preventDefault();
+  let val = $indexInput.value;
+
+  superagent
+    .get('/request')
+    .query({ val })
+    .end( (err, res) => {
+      console.log(res.body);
+    })
+
+  // $indexInput.value = '';
 }
 
-welcome();
+$indexForm.addEventListener('submit', handleFormSubmit);
