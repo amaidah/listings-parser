edcda27f9e48d511832f1029dcd57253fc9670e7
Andrew Maidah <a.maidah@gmail.com>
2017-10-16 00:59:04 -0700

    adds cc_accepted and finishes both category parsing
    Signed-off-by: Andrew Maidah <a.maidah@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('/request', (req, res, next) => {
     // load html
     let ch = cheerio.load(file, { normalizeWhitespace: true });
 
-    //start parse
+    // start parse
     let full_name = ch('.biz-page-title').html().trim();
     parsed.full_name = full_name;
     let budget = ch('.business-attribute, .price-range').html().trim();
@@ -48,6 +48,7 @@ router.get('/request', (req, res, next) => {
     let phone_number = ch('.biz-phone').html().trim();
     parsed.phone_number = phone_number.replace(/\D+/g, "");
 
+    // address
     let address = ch('.street-address > address').html();
     formatAddress(address, formattedAddress => {
       parsed.address = formattedAddress;
@@ -60,16 +61,35 @@ router.get('/request', (req, res, next) => {
     }
 
     // categories
-    let cat = ch('.category-str-list').children();
-    console.log('cat length: ', cat.length);
-
+    const cat = ch('.category-str-list').children();
+    const catArr = [];
     for (let i = 0; i < cat.length; i++) {
-      console.log(cat.eq(i).html())
+      catArr.push(cat.eq(i).html());
     }
-    // ^^^^^^^^^^^^^^^^^^^^^ stops work here
-    cat.each( function(i) {
-      console.log( 'going through: ', ch(this).html(), i );
-    })
+
+    // validate that categories exist
+    parsed.primary = catArr[0] ? catArr[0] : null;
+    parsed.secondary = catArr[1] ? catArr[1] : null;
+
+    // credit card check
+    let bizDataTable = ch('.attribute-key').toArray();
+    let cc_accepted = null;
+
+    // use es6 for.. of to allow break on first match
+    for (let node of bizDataTable) {
+      if (ch(node).html().trim() === 'Accepts Credit Cards') {
+        // save closest 'dl' parent to get access to inner text nodes
+        cc_accepted = ch(node).closest('dl');
+        break;
+      }
+    }
+
+    // validate cc_accepted exists and 'dd' child exists
+    if (cc_accepted !== null && cc_accepted.children('dd') !== null) {
+      // get value from 'dd' child, trim, and store
+      cc_accepted = cc_accepted.children('dd').text().trim();
+    }
+    parsed.cc_accepted = cc_accepted;
 
     resObj.parsed = parsed;
     res.send(resObj);
